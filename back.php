062410a8149f57c38e2c9096cde149d9172b9ea5
JunDa LUO <jluo15@trusty>
2015-11-24 21:20:36 -0500

    fixed cross domain issues

--- back.php
+++ back.php
@@ -1,34 +1,30 @@
 <?php 
+			header('Access-Control-Allow-Origin: *');
+			header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+			header('Access-Control-Max-Age: 1000');
+			header('Access-Control-Allow-Headers: Content-Type');
 
-	// require_once 'include/sqAES.php';
-	// require_once 'include/JCryption.php';
+			$data = json_encode(array("your_request_was" => $_POST['code']));
+			//echo $data;
 
-	// $postBefore = print_r($_POST, true);
+			 // $data = "#include<stdio.h> \n int main(){printf(\"Hello World\"); return 0;}";
+			//$myfile = fopen("something.txt", "w");
 
-	// JCryption::decrypt();
+			// lock to prevent anyone else to write 
+			// to file at the same time, extra safety
+			// file_put_contents($file, $library, FILE_APPEND | LOCK_EX); 
+			file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
+			$consoleMsg = shell_exec('gcc -o jeremysucks userProgram.c');
+			print $consoleMsg;
 
-	// header('Content-type: text/plain');
-	// echo "Original POST\n======================\n";
-	// print_r($postBefore);
-	// echo "jCryption example form\n======================\n";
-	// print_r($_POST);
+			 if( $consoleMsg ){
 
+			$output = shell_exec('./jeremysucks');
+			$arr = array('error1' => consoleMsg, 'error2' => output );
+			 $json = json_encode($arr);
+			 }
+			//print $arr;
+			 file_put_contents("./test.txt", $json);
 
-   	$arr = array('Library'=>"#include<stdio.h> \n int main(){printf(\"Hello World\"); return 0;}");
-   	$json = json_encode($arr);
-
-   	$obj = json_decode($json);
-   	$library = $obj->{'Library'};
-
-	$file = "userProgram.c";
-
-	// lock to prevent anyone else to write 
-	// to file at the same time, extra safety
-	file_put_contents($file, $library, FILE_APPEND | LOCK_EX); 
-	$consoleMsg = shell_exec('gcc -o jeremysucks userProgram.c');
-	print $consoleMsg;
-	$output = shell_exec('./jeremysucks');
-	print $output;
-
-
-?> 	
+			echo $json;
+		?> 	
