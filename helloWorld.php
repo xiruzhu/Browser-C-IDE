5831e56646f720f7d171c834363cc85bee286de7
Xiru <xiruzhu@yahoo.com>
2015-11-25 19:07:08 -0500

    fuck life
    Signed-off-by: Xiru <xiruzhu@yahoo.com>

---
+++ helloWorld.php
@@ -0,0 +1,27 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+    echo "Hello World\n";
+    echo "{".json_encode("Hello World")."}";
+    echo "\n";
+
+    $data = "#include <stdio.h> \n int main(){\nprintf(\"Hello World\");\n void * test = malloc(5);\n test += 5;\n return 0;\n}";
+	$myfile = "userProgram.c";
+	file_put_contents($myfile, $data, LOCK_EX);
+
+
+	$process = proc_open('gcc -Wall -o malloc malloc.c',
+    array(
+        1 => array("pipe", "w"),  //stdout
+        2 => array("pipe", "w")   // stderr
+    ), $pipes);
+
+	echo stream_get_contents($pipes[2]);
+
+	$output = exec("./malloc 2>&1");
+
+	echo "Output:".$output;
+	echo "TEST";
+?>
